4043d3fc65f969f79ef7263fa889b6675d0363f6
Jens Sager <jens.sager@offis.de>
2023-07-28 12:23:58 +0200

    add type forwarding to bson_read and add some more tests

--- src/util/encode_decode.jl
+++ src/util/encode_decode.jl
@@ -1,13 +1,4 @@
 # Not called Codecs because Codecs.jl already exists and I didn't want to deal with that for now.
-
-#=
-The result of the benchmark test was:
-- JSON - simple, slow, converts structs to dicts
-- CBOR - slightly faster but still slow, can reconstruct structs for you if definitions match -> convenient
-- LightBSON - fast (10x faster than JSON), bit more hassle with types
-
-Since our primary focus with Mango.jl is speed improvements over python, the primary codec is LightBSON based for now.
-=#
 module EncodeDecode
 using LightBSON
 using OrderedCollections
@@ -18,16 +9,25 @@ struct MsgContent
     payload::Vector{UInt8}
 end
 
-# the core functions
-# these versions will reduce structs to OrderedDicts with their field names as is LightBSON default
-function encode(data::OrderedDict{String,Any})::Vector{UInt8}
+#= 
+These versions will reduce structs to OrderedDicts with their field names as is LightBSON default.
+"Normal" operation is passing OrderedDict with String keys and basic data type fields.
+
+For convenience, we pass the type info to the bson_read function so you CAN pass structs to and from
+the codec and get your desired type inference.
+This is entirely limited by what bson_read will handle for the types and we make no guarantees on
+the results.
+
+Advanced features like nested smart type inference and such are kept as future work.
+=#
+function encode(data::Union{OrderedDict{String,Any},Any})::Vector{UInt8}
     buf = Vector{UInt8}()
     LightBSON.bson_write(buf, data)
     return buf
 end
 
-function decode(buf::Vector{UInt8})::OrderedDict{String,Any}
-    return LightBSON.bson_read(OrderedDict{String,Any}, buf)
+function decode(buf::Vector{UInt8}, t::Type=OrderedDict{String,Any})::Union{OrderedDict{String,Any},Any}
+    return LightBSON.bson_read(t, buf)
 end
 
 
--- test/encode_decode_tests.jl
+++ test/encode_decode_tests.jl
@@ -10,40 +10,48 @@ import Base.==
 struct MyComposite
     x::Float64
     y::String
-    z::Dict{String,Int64}
-end
-
-struct MyNested
-    a::MyComposite
-    b::MyComposite
-    c::MyComposite
-    s::Union{MyNested,Nothing}
+    z::OrderedDict{String,Any}
 end
 
 function ==(x::MyComposite, y::MyComposite)
     return x.x == y.x && x.y == y.y && x.z == y.z
 end
 
-function ==(x::MyNested, y::MyNested)
-    return x.a == y.a && x.b == y.b && x.c == y.c && x.s == y.s
-end
-
-
 LightBSON.bson_simple(::Type{MyComposite}) = true
 LightBSON.bson_simple(::Type{MyNested}) = true
 
 
 @testset "EncodeDecode" begin
-    test_dict = OrderedDict(
+    #= things that will not pass the simple equality check:
+        - imaginary numbers (get cast to ordered dicts with "re" and "im" keys)
+        - nested structures
+        - anything self referential
+    =#
+    test_dict1 = OrderedDict(
         [
-        ("1" => 1.0),
-        ("2" => "two"),
-        ("3" => 10)
+        "1" => 1.0,
+        "2" => "two",
+        "3" => 1,
+        "4" => [1, 2, 3, 5, 6, 7],
+        "5" => [1.0, 2.0, Inf, NaN],
+        "6" => ["a", "b", "c", "d"],
+        "7" => Any["a", 1.0, NaN, Inf, zeros(Float64, 10)],
+        "8" => Any[OrderedDict{String,Any}(["a" => "nested"])]
     ]
     )
 
-    encoded = EncodeDecode.encode(test_dict)
-    decoded = EncodeDecode.decode(encoded)
+    test_dict2 = OrderedDict{String,Any}()
 
-    @test test_dict == decoded
+    encoded = EncodeDecode.encode(test_dict1)
+    decoded = EncodeDecode.decode(encoded)
+    @test isequal(decoded, test_dict1)
+
+    encoded = EncodeDecode.encode(test_dict2)
+    decoded = EncodeDecode.decode(encoded)
+    @test isequal(decoded, test_dict2)
+
+    composite = MyComposite(10.0, "some string", OrderedDict(["abc" => 123, "def" => 456]))
+    encoded = EncodeDecode.encode(composite)
+    decoded = EncodeDecode.decode(encoded, MyComposite)
+    @test isequal(decoded, composite)
 end
