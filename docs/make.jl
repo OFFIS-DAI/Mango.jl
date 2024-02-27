4ed44a553b02c197c1c62c25b0664103148604bf
Rico Schrage <rico.schrage@uni-oldenburg.de>
2024-02-27 17:28:42 +0100

    Documentation publishing.

--- docs/make.jl
+++ docs/make.jl
@@ -7,12 +7,15 @@ makedocs(
     format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true"),
     authors="mango Team",
     sitename="Mango.jl",
-    pages=Any["Home" => "index.md",
-              "Getting Started" => "getting_started.md",
-              "Agents" => "agent.md",
-              "Container" => "container.md",
-              "Codecs" => "encode_decode.md",
-              "Scheduling" => "scheduling.md",
-              "Legals" => "legals.md",],
-    repo = "https://gitlab.com/mango-agents/Mango.jl",
+    pages=Any["Home"=>"index.md",
+        "Getting Started"=>"getting_started.md",
+        "Agents"=>"agent.md",
+        "Container"=>"container.md",
+        "Codecs"=>"encode_decode.md",
+        "Scheduling"=>"scheduling.md",
+        "Legals"=>"legals.md",],
+    repo="https://github.com/OFFIS-DAI/Mango.jl",
+)
+deploydocs(
+    repo="github.com/OFFIS-DAI/Mango.jl.git",
 )
