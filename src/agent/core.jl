35d9bc1ed0744841f6947d434a7682e6c05528fa
Rico Schrage <rico.schrage@uni-oldenburg.de>
2024-06-17 14:54:03 +0200

    Docs

--- src/agent/core.jl
+++ src/agent/core.jl
@@ -370,6 +370,15 @@ function send_tracked_message(
     return send_message(agent, content, AgentAddress(agent_address.aid, agent_address.address, tracking_id); kwargs...)
 end
 
+"""
+Convenience method to reply to a received message using the meta the agent received. This reduces the regular send_message as response
+`send_message(agent, "Pong", AgentAddress(aid=meta["sender_id"], address=meta["sender_addr"]))`
+to
+`reply_to(agent, "Pong", meta)`
+
+Furthermore it guarantees that agent address (including the tracking id, which is part of the address!) is correctly passed to the mango
+container.
+"""
 function reply_to(agent::Agent,
     content::Any,
     received_meta::AbstractDict;
