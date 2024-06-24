7fdf6283f6d6d6da903808a77c40cb17563a1a2c
Jens Sager <jens.sager@uni-oldenburg.de>
2024-06-24 19:48:43 +0200

    add MQTT testset

--- test/examples.jl
+++ test/examples.jl
@@ -75,6 +75,65 @@ end
     @test ping_agent.counter >= 5
 end
 
+
+# Define the ping pong agent
+@agent struct MQTTPingPongAgent
+    counter::Int
+end
+
+# Override the default handle_message function for ping pong agents
+function handle_message(agent::MQTTPingPongAgent, message::Any, meta::Any)
+    broker_addr = agent.context.container.protocol.broker_addr
+
+    if message == "Ping"
+        agent.counter += 1
+        send_message(agent, "Pong", MQTTAddress(broker_addr, "pongs"))
+    elseif message == "Pong"
+        agent.counter += 1
+        send_message(agent, "Ping", MQTTAddress(broker_addr, "pings"))
+    end
+end
+
 @testset "MQTT_AGENT" begin
-    
+    broker_addr = InetAddr(ip"127.0.0.1", 1883)
+
+    c1 = Container()
+    c1.protocol = MQTTProtocol("PingContainer", broker_addr)
+
+    c2 = Container()
+    c2.protocol = MQTTProtocol("PongContainer", broker_addr)
+
+    # Define the ping pong agent
+    # Create instances of ping pong agents
+    ping_agent = MQTTPingPongAgent(0)
+    pong_agent = MQTTPingPongAgent(0)
+
+    # register each agent to a container
+    # For the MQTT protocol, topics for each agent have to be passed here.
+    register(c1, ping_agent; topics=["pongs"])
+    register(c2, pong_agent; topics=["pings"])
+
+    # Start the container
+    wait(Threads.@spawn start(c1))
+    wait(Threads.@spawn start(c2))
+
+    # Send the first message to start the exchange
+    wait(send_message(ping_agent, "Ping", MQTTAddress(broker_addr, "pings")))
+
+    # Wait for a moment to see the result
+    # In general you want to use a Condition() instead to
+    # Define a clear stopping signal for the agents
+    wait(Threads.@spawn begin
+        while ping_agent.counter < 5 
+            sleep(1)
+        end
+    end)
+
+    # Start the container
+    @sync begin
+        Threads.@spawn shutdown(c1)
+        Threads.@spawn shutdown(c2)
+    end
+
+    @test ping_agent.counter >= 5
 end
