4783db7a663da853170700f93167daa6874491e2
Jens Sager <jens.sager@uni-oldenburg.de>
2024-06-24 19:05:46 +0200

    add mqtt protocol docstrings

--- src/container/mqtt.jl
+++ src/container/mqtt.jl
@@ -11,7 +11,6 @@ export MQTTProtocol,
 using Mosquitto
 using Sockets: InetAddr
 
-# one protocol one broker for now?
 # TODO TLS and connection handling for that
 
 mutable struct MQTTProtocol <: Protocol{String}
@@ -24,7 +23,6 @@ mutable struct MQTTProtocol <: Protocol{String}
 
     function MQTTProtocol(client_id::String, broker_addr::InetAddr)
         # Have to cast types for the MQTT client constructor.
-        # TODO: maybe suggest a constructor using InetAddr to the Mosquitto repo?
         c = Client(string(broker_addr.host), Int64(broker_addr.port); id=client_id)
         msg_channel = get_messages_channel(c)
         conn_channel = get_connect_channel(c)
@@ -32,6 +30,14 @@ mutable struct MQTTProtocol <: Protocol{String}
     end
 end
 
+"""
+Initialize the Mosquitto looping task. This checks for incoming messages and 
+forwards their content to the container.
+
+# Returns
+
+Created Tasks
+"""
 function init(protocol::MQTTProtocol, stop_check::Function, data_handler::Function)
     tasks = []
     listen_task = errormonitor(
@@ -61,20 +67,33 @@ function init(protocol::MQTTProtocol, stop_check::Function, data_handler::Functi
         end
     )
     
-    
-
     return listen_task, tasks
 end
 
+"""
+Send a message `message` to topic `destination` on the clients MQTT broker . 
+
+# Returns
+Return value and message id from MQTT library.
+"""
 function send(protocol::MQTTProtocol, destination::String, message::Any)
     publish(protocol.client, destination, message)
 end
 
+"""
+Returns the namethe client is registered with at its broker.
+"""
 function id(protocol::MQTTProtocol)
     return protocol.client.id
 end
 
-function is_connected(protocol::MQTTProtocol)::Bool
+"""
+Polls the connection channel and updates the protocols internal connection flag accordingly.
+
+# Returns
+The connection status as bool.
+"""
+function is_connected!(protocol::MQTTProtocol)::Bool
     while !isempty(protocol.conn_channel)
         conncb = take!(protocol.conn_channel)
 
@@ -88,12 +107,18 @@ function is_connected(protocol::MQTTProtocol)::Bool
     return protocol.connected
 end
 
+"""
+Subscribe the MQTT client of the protocol to `topic`.
+"""
 function subscribe(protocol::MQTTProtocol, topic::String; qos::Int=1)
     Mosquitto.subscribe(protocol.client, topic; qos)
 end
 
+"""
+Disconnect the client from the broker and stop the message loop.
+"""
 function close(protocol::MQTTProtocol)
-    if is_connected(protocol)
+    if is_connected!(protocol)
         disconnect(protocol.client)
         Mosquitto.loop_stop(protocol.client)
     end
@@ -103,6 +128,12 @@ function parse_id(_::MQTTProtocol, id::Any)::String
     return string(id)
 end
 
+
+"""
+Notify the client that a new agent has been registered. Registrations expects a kwarg `topics` 
+to subscribe the agent to. 
+If any topic is not yet subscribed by the client `subscribe` is called for it.
+"""
 function notify_register(protocol::MQTTProtocol, aid::String; kwargs...)
     topics = :topics in keys(kwargs) ? kwargs[:topics] : []
 
@@ -117,5 +148,4 @@ function notify_register(protocol::MQTTProtocol, aid::String; kwargs...)
             push!(protocol.topic_to_aid[topic], aid)
         end
     end
-    
 end
